c2d7a4985eaadc21e94ec3205a82417347e8bf64
David Crowley <david.crowley@camunda.comd>
2022-06-12 15:34:02 +0200

    Exercise 8

---
+++ Exercise 8/Java/src/main/java/com/camunda/training/payment/CreditCardHandler.java
@@ -0,0 +1,51 @@
+package com.camunda.training.payment;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.camunda.bpm.client.task.ExternalTaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.camunda.training.services.CreditCardService;
+
+@Component
+@ExternalTaskSubscription("creditCardCharging")
+public class CreditCardHandler implements ExternalTaskHandler {
+	
+	@Autowired
+	public CreditCardService service;
+	
+	private static final Logger LOG = LoggerFactory.getLogger(CreditCardHandler.class);
+	
+	@Override
+	public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+		
+		LOG.info("handle topic {} for task id {}", externalTask.getTopicName(), externalTask.getId());
+		
+	    String cardNumber = externalTask.getVariable("cardNumber"), 
+	    		      cvc = externalTask.getVariable("CVC"), 
+	    	   expiryDate = externalTask.getVariable("expiryDate");
+	    
+	    Double openAmount = externalTask.getVariable("openAmount");
+	    
+	    try {
+	    	service.chargeAmount(cardNumber, cvc, expiryDate, openAmount);
+	    	
+			externalTaskService.complete(externalTask);
+	    } catch (IllegalArgumentException e) {
+	        StringWriter sw = new StringWriter();
+	        
+	        e.printStackTrace(new PrintWriter(sw));
+	        
+	        externalTaskService.handleFailure(externalTask, "credit card expired", sw.toString(), 0, 0);
+	    }	
+
+	}
+	
+}
---
+++ Exercise 8/Java/src/main/java/com/camunda/training/payment/PaymentCompletionHandler.java
@@ -0,0 +1,49 @@
+package com.camunda.training.payment;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.camunda.bpm.client.task.ExternalTaskService;
+import org.camunda.community.rest.client.api.MessageApi;
+import org.camunda.community.rest.client.dto.CorrelationMessageDto;
+import org.camunda.community.rest.client.dto.MessageCorrelationResultWithVariableDto;
+import org.camunda.community.rest.client.dto.VariableValueDto;
+import org.camunda.community.rest.client.invoker.ApiClient;
+import org.camunda.community.rest.client.invoker.ApiException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@ExternalTaskSubscription("paymentCompletion")
+public class PaymentCompletionHandler implements ExternalTaskHandler {
+      
+	private static final Logger LOG = LoggerFactory.getLogger(PaymentCompletionHandler.class);
+	
+	@Override
+	public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+		LOG.info("handle topic {} for task id {}", externalTask.getTopicName(), externalTask.getId());
+		
+	    Map<String, VariableValueDto> correlationKey = new HashMap<>();
+	    
+	    correlationKey.put("paymentProcessInstanceId", new VariableValueDto().value(externalTask.getProcessInstanceId()));
+	    
+	    ApiClient client = new ApiClient();
+	    
+	    try {
+	    	
+	    	List<MessageCorrelationResultWithVariableDto> correlationResult = new MessageApi(client).deliverMessage(new CorrelationMessageDto().messageName("paymentCompletedMessage").correlationKeys(correlationKey).resultEnabled(true));
+	    	
+	    	LOG.info("correlation result: {}", correlationResult);
+	    	
+	    } catch (ApiException e) {
+	    	e.printStackTrace();
+	    }
+
+	    externalTaskService.complete(externalTask);
+    }
+}
