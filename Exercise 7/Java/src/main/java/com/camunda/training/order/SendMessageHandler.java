5efc3c65b8f9d0cf98643e1bb3c917d89c336584
Joeri Diederen <63590464+joeridiederen@users.noreply.github.com>
2022-07-06 15:07:26 +0200

    Update SendMessageHandler.java
    Fix for the map error.

--- Exercise 7/Java/src/main/java/com/camunda/training/order/SendMessageHandler.java
+++ Exercise 7/Java/src/main/java/com/camunda/training/order/SendMessageHandler.java
@@ -1,9 +1,6 @@
-package com.camunda.training.order;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
 import org.camunda.bpm.client.task.ExternalTask;
 import org.camunda.bpm.client.task.ExternalTaskHandler;
@@ -22,35 +19,33 @@ import org.springframework.stereotype.Component;
 @ExternalTaskSubscription("paymentRequest")
 public class SendMessageHandler implements ExternalTaskHandler {
 
-	private static final Logger LOG = LoggerFactory.getLogger(SendMessageHandler.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SendMessageHandler.class);
 
-	@Override
-	public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
-		LOG.info("handle topic {} for task id {}", externalTask.getTopicName(), externalTask.getId());  
+    @Override
+    public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+        LOG.info("handle topic {} for task id {}", externalTask.getTopicName(), externalTask.getId());
+        ApiClient client = new ApiClient();
+        String paymentProcessInstanceId = null;
 
-	    Map<String, VariableValueDto> payload = new HashMap<>();
-	    
-	    for (Map.Entry<String, Object> variable : externalTask.getAllVariables().entrySet()) {
-	    	payload.put(variable.getKey(), new VariableValueDto().value(variable.getValue()));
-	    }
+        Map<String, VariableValueDto> payload = new HashMap<>();
+        for (Map.Entry<String, Object> variable : externalTask.getAllVariables().entrySet()) {
+            payload.put(variable.getKey(), new VariableValueDto().value(variable.getValue()));
+        }
 
-	    String paymentProcessInstanceId = "";
-	    
-		ApiClient client = new ApiClient();
-	    
-	    try {
-	    	
-	    	List<MessageCorrelationResultWithVariableDto> correlationResult = new MessageApi(client).deliverMessage(new CorrelationMessageDto().messageName("paymentRequestMessage").businessKey(externalTask.getBusinessKey()).processVariables(payload).resultEnabled(true));
-	    	
-	    	LOG.info("correlation result: {}", correlationResult);
-	    	
-	    	paymentProcessInstanceId = correlationResult.get(0).getProcessInstance().getId();
-	    	
-	    } catch (ApiException e) {
-	    	e.printStackTrace();
-	    }
+        try {
+            List<MessageCorrelationResultWithVariableDto> correlationResult = new MessageApi(client)
+                    .deliverMessage(new CorrelationMessageDto().messageName("paymentRequestMessage")
+                            .businessKey(externalTask.getBusinessKey()).processVariables(payload).resultEnabled(true));
+            LOG.info("correlation result: {}", correlationResult);
+            paymentProcessInstanceId = correlationResult.get(0).getProcessInstance().getId();
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
 
-	    externalTaskService.complete(externalTask, Map.of("paymentProcessInstanceId", paymentProcessInstanceId));
-	}
-	
+        Map<String, Object> varMap = new HashMap<String, Object>();
+
+        varMap.put("paymentProcessInstanceId", paymentProcessInstanceId);
+
+        externalTaskService.complete(externalTask, varMap);
+    }
 }
